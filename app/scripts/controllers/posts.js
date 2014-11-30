ba71342af6f75cd6a5f24f15336ecf12531336e0
Pinakin Mistry <pinakin.nmistry@gmail.com>
2014-11-30 15:39:45 +0530

    Hiding elements from unauthenticated users

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,7 +1,8 @@
 'use strict';
 
-app.controller('PostsCtrl', function ($scope, Post, $location) {
+app.controller('PostsCtrl', function ($scope, Post, $location, Auth) {
     $scope.posts = Post.all;
+    $scope.user = Auth.user;
 
     $scope.post = {url: 'http://', title: ''};
 
